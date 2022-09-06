46f46d78523b90ead41b3a29e40cda4b394c346a
Jose Molina <josemolinaf22@gmail.com>
2022-09-06 12:12:41 -0600

    changed back

--- server/index.js
+++ server/index.js
@@ -20,8 +20,8 @@ app.get("/api/bars/:id", getBars);
 app.post("/api/verifyUser", verifyUser);
 app.post("/api/registerUser", registerUser);
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 
 app.listen(PORT || 4000, () => console.log(`Up on ${PORT}`));
